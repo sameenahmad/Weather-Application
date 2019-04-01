a28350161d64127707a61a140469bac23c2e4039
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-04-01 15:43:23 +0530

    weather App with complete js

--- promise.js
+++ promise.js
@@ -1,8 +1,20 @@
-const sayHello = (name) => {
-
-    return new Promise((resolve, reject)=>{
-            resolve(`Hello, ${name}!`);
-    })
-
-
+var tempValue=document.getElementById('tempValue');
+var minTemp=document.querySelector('.minTemp');
+var maxTemp=document.querySelector('.maxTemp');
+ function updateTemp()
+ { fetch('http://api.openweathermap.org/data/2.5/weather?id=1261481&units=metric&APPID=1b9eec515d18cf2e35e977219ce02cc4')
+.then((res)=>res.json())
+ .then((data)=>{
+  tempValue.innerHTML= data.main.temp;
+  minTemp.innerHTML=data.main.temp_min;
+  maxTemp.innerHTML=data.main.temp_max;
+})
 }
+updateTemp();
+
+
+
+
+
+
+//api.openweathermap.org/data/2.5/weather?id=1261481&APPID=1b9eec515d18cf2e35e977219ce02cc4'
