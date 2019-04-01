e2c7ec590ae77c71c6d0177d8951c959563a7b21
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-04-01 16:06:52 +0530

    updated Description

--- promise.js
+++ promise.js
@@ -1,6 +1,9 @@
 var tempValue=document.getElementById('tempValue');
 var minTemp=document.querySelector('.minTemp');
 var maxTemp=document.querySelector('.maxTemp');
+var weatherDescription=document.getElementById('weatherDescription')
+var weatherIcon=document.getElementById('weatherIcon')
+
  function updateTemp()
  { fetch('http://api.openweathermap.org/data/2.5/weather?id=1261481&units=metric&APPID=1b9eec515d18cf2e35e977219ce02cc4')
 .then((res)=>res.json())
@@ -8,6 +11,7 @@ var maxTemp=document.querySelector('.maxTemp');
   tempValue.innerHTML= data.main.temp;
   minTemp.innerHTML=data.main.temp_min;
   maxTemp.innerHTML=data.main.temp_max;
+  weatherDescription.innerHTML=data.weather[0].description;
 })
 }
 updateTemp();
