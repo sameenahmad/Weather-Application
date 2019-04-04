6a4b2a0ce0f666a062e8af2ae27f75e5da05a9c3
Sameen Ahmad <sameen.ahmad21@gmail.com>
2019-04-04 17:36:09 +0530

    Updated celcius/Kelvin/F Buttons

--- promise.js
+++ promise.js
@@ -3,22 +3,46 @@ var minTemp=document.querySelector('.minTemp');
 var maxTemp=document.querySelector('.maxTemp');
 var weatherDescription=document.getElementById('weatherDescription')
 var weatherIcon=document.getElementById('weatherIcon')
+var celcius=document.getElementById('celcius')
+var kelvin=document.getElementById('kelvin')
+var fahrenheit=document.getElementById('fahrenheit')
+
+ function getUnit()
+ { unit='metric';
+   celcius.addEventListener('click', ()=>
+   {
+     unit='metric';
+     getTemp();
+   })  
+   kelvin.addEventListener('click', ()=>{
+     unit='';
+     getTemp();
+   })
+   fahrenheit.addEventListener('click',()=>{
+     unit='imperial';
+     getTemp();
+   })
+ return unit;
+  }
 
 function getTemp()
 {
- var searchValue=document.getElementById('searchValue').value;
-  console.log(searchValue);
-  updateTemp(searchValue);
+ var location=document.getElementById('searchValue').value;
+ updateTemp(location);
 }
+var api='https://api.openweathermap.org/data/2.5/weather?q='
+var unit=getUnit();
+var appid='&appid=1b9eec515d18cf2e35e977219ce02cc4';
 
  function updateTemp(location)
- { fetch('https://api.openweathermap.org/data/2.5/weather?q='+ location + ',ind&units=metric&appid=1b9eec515d18cf2e35e977219ce02cc4')
+ { fetch(api+ location + '&units=' + unit + appid) 
 .then((res)=>res.json())
  .then((data)=>{
   tempValue.innerHTML= data.main.temp;
   minTemp.innerHTML=data.main.temp_min;
   maxTemp.innerHTML=data.main.temp_max;
   weatherDescription.innerHTML=data.weather[0].description;
+  console.log(data)
   var icon=data.weather[0].description;
   console.log(icon);
   switch(icon)
@@ -27,7 +51,7 @@ function getTemp()
     break;
     case 'clear sky':
     {
-      weatherIcon.src='clearsky.png'
+      weatherIcon.src='clearsky.svg'
     }
     break;
     case 'overcast clouds':
@@ -45,9 +69,6 @@ function getTemp()
       weatherIcon.src='default.gif'
     }
   }
-
-
-  
 })
 .catch(()=>console.log("OOOPS!!LOCATION NOT FOUND"))
 }
